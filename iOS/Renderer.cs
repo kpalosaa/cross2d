4d768a7821479562e6269eee8d78268ffbf3efdc
Kimmo Palosaari <kpalosaa@gmail.com>
2016-07-06 23:59:11 +0300

    Added iOS

---
+++ iOS/Renderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Uni2D
+{
+	public abstract class Renderer<TView> : Xamarin.Forms.Platform.iOS.ViewRenderer<TView, View> where TView : Xamarin.Forms.View
+	{
+		protected override void OnElementChanged(Xamarin.Forms.Platform.iOS.ElementChangedEventArgs<TView> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control == null)
+			{
+				View view = new View();
+				SetNativeControl(view);
+			}
+
+			if (e.OldElement != null)
+			{
+				Control.DrawView -= OnDrawView;
+			}
+
+			if (e.NewElement != null)
+			{
+				Control.DrawView += OnDrawView;
+			}
+		}
+
+		private void OnDrawView(object sender, DrawViewEventArgs e)
+		{
+			Context context = new Context(e.Context, e.Rect);
+			context.Width = (float)Element.Width;
+			context.Height = (float)Element.Height;
+
+			Draw(context);
+		}
+
+		protected abstract void Draw(IContext context);
+	}
+}
---
+++ iOS/View.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace Uni2D
+{
+	public class View : UIView
+	{
+		public event DrawViewEventHandler DrawView;
+
+		public override void Draw(CGRect rect)
+		{
+			base.Draw(rect);
+
+			var width = Bounds.Size.Width;
+			var height = Bounds.Size.Height;
+
+			using (var context = UIGraphics.GetCurrentContext())
+			{
+				DrawView?.Invoke(this, new DrawViewEventArgs() { Context = context, Rect = rect });
+			}
+		}
+	}
+
+	public delegate void DrawViewEventHandler(object sender, DrawViewEventArgs e);
+
+	public class DrawViewEventArgs : EventArgs
+	{
+		public CGContext Context { get; set; }
+		public CGRect Rect { get; set; }
+	}
+}
