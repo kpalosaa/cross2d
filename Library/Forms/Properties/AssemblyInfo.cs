deeb8fda2ad3a8356d8fa5e6fdbd2b26050013a4
Kimmo Palosaari <kpalosaa@gmail.com>
2016-08-03 12:44:48 +0300

    New folder structure. PCL dll added.

--- Library/Forms/Properties/AssemblyInfo.cs
+++ Library/Forms/Properties/AssemblyInfo.cs
@@ -1,18 +1,20 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Cross2D.Forms")]
+[assembly: AssemblyDescription("Cross platform simple 2D graphics library for Xamarin Forms custom controls")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Cross2D.Forms")]
+[assembly: AssemblyCopyright("Copyright © 2016 Kimmo Palosaari")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -24,6 +26,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+[assembly: InternalsVisibleTo("Cross2D.Droid")]
+[assembly: InternalsVisibleTo("Cross2D.iOS")]
+[assembly: InternalsVisibleTo("Cross2D.UWP")]
--- Library/iOS/Properties/AssemblyInfo.cs
+++ Library/iOS/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("iOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Cross2D.iOS")]
+[assembly: AssemblyDescription("Cross platform simple 2D graphics library for Xamarin Forms custom controls")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("kpalosaa")]
+[assembly: AssemblyProduct("Cross2D.iOS")]
+[assembly: AssemblyCopyright("Copyright © 2016 Kimmo Palosaari")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
